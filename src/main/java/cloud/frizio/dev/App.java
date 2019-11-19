2186a1d81e42697522fe25483adfb979046b1178
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2019-11-19 10:41:55 +0100

    first commit - JUnit 4 vs JUnit-Jupiter 5

---
+++ src/main/java/cloud/frizio/dev/App.java
@@ -0,0 +1,8 @@
+package cloud.frizio.dev;
+
+
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "TDD Demo" );
+    }
+}
---
+++ src/test/java/cloud/frizio/dev/AppTest.java
@@ -0,0 +1,22 @@
+package cloud.frizio.dev;
+
+/*
+import org.junit.Test;
+import static org.junit.Assert.fail;
+import static org.junit.Assert.assertTrue;
+*/
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+
+public class AppTest {
+
+  @Test
+  public void test() {
+    fail("Not Yet implemented");
+    // assertTrue( true );
+  }
+  
+}
