c14ee82188e0970fbbfb3009e1192097cc0f5c76
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2019-11-22 10:40:49 +0100

    Cleant test class code

--- src/test/java/cloud/frizio/dev/isbntools/StackManagementTest.java
+++ src/test/java/cloud/frizio/dev/isbntools/StackManagementTest.java
@@ -9,16 +9,15 @@ public class StackManagementTest {
   @Test
   public void canGetAcorrectLocatorCode() {
 
-    // MOCKUP of the external WEB service
+    // STUB of the external WEB service
     ExternalISBNDataService webService = new ExternalISBNDataService() {
       @Override
       public Book lookup(String isbn) {
-        // This is a test STUB: dependency required bu the testing method
         return new Book(isbn, "Of Mice and Man", "J. Steinbeck");
       }
     };
 
-    // MOCKUP of the external DATABASE service
+    // STUB of the external DATABASE service
     ExternalISBNDataService databaseService = new ExternalISBNDataService() {
       @Override
       public Book lookup(String isbn) {
@@ -34,6 +33,5 @@ public class StackManagementTest {
     String locatorCode = stockManager.getLocatorCode(isbn);
     assertEquals("7396J4", locatorCode);
   }
-
 
 }
